afbb7328e1b030792a463e29193a5b1c10931db1
Kannan Duraisamy <currentkannan@gmail.com>
2026-07-17 07:28:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+
+import './App.css'
+
+function App() {
+ 
+
+
+
+
+
+
+  return (
+    <>
+    <Header />
+    <Home />
+    <Footer />
+     
+    </>
+  )
+}
+
+export default App
