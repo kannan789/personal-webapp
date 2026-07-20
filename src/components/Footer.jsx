37ca222bcd4aa057bcc3f514fdbba493e2af9f5b
Kannan Duraisamy <currentkannan@gmail.com>
2026-07-20 07:45:12 +0530

    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,12 +20,12 @@ export default function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <Link to="/" className="hover:underline">
+                                    <Link to="/personal-webapp/" className="hover:underline">
                                         Home
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="/about" className="hover:underline">
+                                    <Link to="/personal-webapp/about" className="hover:underline">
                                         About
                                     </Link>
                                 </li>
